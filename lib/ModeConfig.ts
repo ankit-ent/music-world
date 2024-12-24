1f7f2a94521dfb5779769966b9635e5359162541
ankit-ent <info@ankitgoa.com>
2024-12-24 15:02:45 +0530

    Linting error fix

--- lib/ModeConfig.ts
+++ lib/ModeConfig.ts
@@ -1,12 +1,12 @@
-interface NoteProperties {
-  isActive: boolean;
-  volume: number;
-  prob: number;
-  color: string;
-  intervals?: number[];
-}
+// interface NoteProperties {
+//   isActive: boolean;
+//   volume: number;
+//   prob: number;
+//   color: string;
+//   intervals?: number[];
+// }
 
-type ScaleConfig = NoteProperties[];
+// type ScaleConfig = NoteProperties[];
 
 // const MajorScale: ScaleConfig = [
 //   // Position:  0-Root  1-m2   2-M2   3-m3   4-M3   5-P4   6-Tri  7-P5   8-m6   9-M6   10-m7  11-M7
